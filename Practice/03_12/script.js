163285164c608725be398c938d8d049120c0ba98
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-11-27 01:06:16 +0100

    Add Laptop class with properties and method for toggling gamingPC state

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,25 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Laptop {
+    constructor(
+        name,
+        color,
+        gamingPC,
+        GPUprod,
+        GPUseries,
+    ) {
+        this.name = name,
+        this.color = color,
+        this.gamingPC = gamingPC,
+        this.GPU = {
+            prod : GPUprod,
+            series : GPUseries
+        };
+    }
+    toggleGamingPC(state) {
+        this.gamingPC = state
+    }
+}
+export default Laptop;
